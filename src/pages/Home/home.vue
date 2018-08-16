1b310c7303a38b7b2491a4691603c3bf82772b42
zh-j <2867186234@qq.com>
2018-08-16 12:02:09 +0800

    project initialized

---
+++ src/pages/Home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <div>home</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+<style>
+  .home{
+    margin: 0 auto;
+    width:200px;
+    height: 200px;
+    background-color: deepskyblue;
+  }
+</style>
