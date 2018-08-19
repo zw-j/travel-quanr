ad0f03f7ce9e66950be1b255d6c25ea4d2dc8dee
zh-j <2867186234@qq.com>
2018-08-19 10:50:58 +0800

    city search

---
+++ src/pages/City/components/search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input type="text" class="input-search" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~style/variblee.styl'
+  .search
+    line-height .72rem;
+    padding .1rem
+    background $bgColor
+    .input-search
+      box-sizing border-box
+      width: 100%
+      line-height .62rem
+      text-align center
+      border-radius .06rem
+      height .62rem
+      border:none
+      padding 0 .1rem
+      color: gray
+    .input-search::-webkit-input-placeholder
+      color #ccc
+</style>
