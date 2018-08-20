1f46d956c28963df85f9b890e3991195894ae37a
zh-j <2867186234@qq.com>
2018-08-20 10:58:00 +0800

    search logic

--- src/pages/City/components/search.vue
+++ src/pages/City/components/search.vue
@@ -1,10 +1,58 @@
 <template>
-  <div class="search">
-    <input type="text" class="input-search" placeholder="输入城市名或拼音">
-  </div>
+  <div>
+    <div class="search">
+      <input type="text" v-model="keyword" class="input-search" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search_flag" v-show="this.keyword">
+      <ul>
+        <li class="search-item" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item" v-show="!this.list.length">没有找到匹配地点</li>
+      </ul>
+    </div>
+    </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null,
+      flag: true
+    }
+  },
+  mounted () {
+    this.BScroll = new Bscroll(this.$refs.search_flag)
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 ||
+                value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  props: {
+    cities: {
+      type: Object
+    }
+  },
   name: 'CitySearch'
 }
 </script>
@@ -26,4 +74,17 @@ export default {
       color: gray
     .input-search::-webkit-input-placeholder
       color #ccc
+  .search-content
+    z-index 1
+    overflow hidden
+    position:absolute
+    top 1.65rem
+    bottom 0;
+    left 0
+    right 0
+    background white
+    .search-item
+      line-height .62rem
+      border-bottom .01rem solid #ccc
+      padding-left .2rem
 </style>
